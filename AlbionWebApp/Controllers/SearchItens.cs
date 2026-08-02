983f8ec255efdab7243b7806dcb1227923cc78f3
paiva <paiva.lucas09@hotmail.com>
2026-08-01 21:08:44 -0300

    Adicionar arquivos de projeto.

---
+++ AlbionWebApp/Controllers/SearchItens.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlbionWebApp.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class SearchItens : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
